bd23bf78b1b767cc18a0c6052a7384b690df2e45
Lakshay Sharma <dev.lakshaysharma@gmail.com>
2025-06-03 13:58:16 +0530

    first commit

--- src/main/java/com/example/oauth_graphql_reactive/controller/MetaAuthController.java
+++ src/main/java/com/example/oauth_graphql_reactive/controller/MetaAuthController.java
@@ -1,7 +1,9 @@
 package com.example.oauth_graphql_reactive.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -9,6 +11,23 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequestMapping("/api/auth/meta")
 public class MetaAuthController {
+
+    private final WebClient webClient = WebClient.create();
+
+    @Value("${CLIENT_ID}")
+    private String CLIENT_ID;
+
+    @Value("${CLIENT_SECRET}")
+    private String CLIENT_SECRET;
+
+    @Value("${SCOPE}")
+    private String SCOPE;
+
+    @Value("${REDIRECT_URL}")
+    private String REDIRECT_URL;
+
+    @Value("${STATE}")
+    private String STATE;
 
     
 
